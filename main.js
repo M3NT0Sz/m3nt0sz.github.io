14c9c08be9e5ba09aec87dafef0a558b87f905dd
M3NT0Sz <matheusmendes2005@outlook.com>
2025-02-28 08:29:28 -0300

    Para celular index

--- main.js
+++ main.js
@@ -4,5 +4,6 @@ $(document).ready(function () {
     $('.menu-btn').click(function () {
         $('.nav-list').toggleClass('active');
         $('.menu-btn').toggleClass('toggle');
+        $('.social').toggleClass('opacity');
     })
 });
