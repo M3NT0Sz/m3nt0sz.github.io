a3d9bfb3d45a79b55d006de5bfcc5bee31c1d242
M3NT0Sz <matheusmendes2005@outlook.com>
2025-02-28 18:15:42 -0300

    Sobre Mim

--- main.js
+++ main.js
@@ -1,5 +1,3 @@
-document.getElementById('backend-dev').innerText = '<Backend Developer/>';
-
 $(document).ready(function () {
     $('.menu-btn').click(function () {
         $('.nav-list').toggleClass('active');
